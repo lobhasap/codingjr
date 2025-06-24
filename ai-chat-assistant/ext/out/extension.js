14d970a4328767ccb2b82621dc51f66820457dec
lobhasap <142318426+lobhasap@users.noreply.github.com>
2025-06-24 16:33:52 +0530

    Restore clean extension files

---
+++ ai-chat-assistant/ext/out/extension.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+function activate(context) {
+    // Your extension activation code here
+}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
